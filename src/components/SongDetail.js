80e1f7716f22bdfb10b4bb2160a7491227082e33
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-01 18:08:26 -0500

    add details on selected song

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
+const SongDetail = ({ song }) => {
+    if (!song) {
+      return <div>Select a song</div>;
+    }
+  
     return (
-        <div>Song Detail</div>
-    )
+      <div>
+        <h3>Details for:</h3>
+        <p>
+          Title: {song.title}
+          <br />
+          Duration: {song.duration}
+        </p>
+      </div>
+    );
 };
 
 const mapStateToProps = state => {
     return { song: state.selectedSong };
 }
+
 export default connect(mapStateToProps)(SongDetail);
